252d3c9e938256a5fb7bcb48f67d44051f359f1e
mengistiab woldearegay <solmengs@gmail.com>
2019-10-17 03:11:47 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,21 @@
 def create_an_empty_array
-  
+  element_array=[]
 end
 
 def create_an_array
   
+  array=["arr"]
 end
 
 def add_element_to_end_of_array(array, element)
+  end_array=["array","element"]
+end_array<<"symbol"
   
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(start, end)
+  start_array=["arra","elemen"]
+  start_array.unshift=["start]
   
 end
 
